8ef85262f2bc15fd4150cc8f31811662bc2515f7
lymaalpha <lymaalpha@gmail.com>
2025-10-05 20:21:05 +0200

    Update marketplaceInstructions.ts

--- src/marketplaceInstructions.ts
+++ src/marketplaceInstructions.ts
@@ -19,4 +19,34 @@ export async function buildExecuteSaleTransaction(params: {
   const sellerTradeState = await metaplex.auctionHouse().findTradeState({
     auctionHouse: auctionHouseObj,
     wallet: payerKeypair.publicKey,
-    tokenMint: new PublicKey
+    tokenMint: new PublicKey(listing.mint!),
+    tokenSize: 1,
+    price: listing.price!
+  });
+
+  const buyerTradeState = await metaplex.auctionHouse().findTradeState({
+    auctionHouse: auctionHouseObj,
+    wallet: new PublicKey(bid.bidderPubkey || payerKeypair.publicKey.toString()),
+    tokenMint: new PublicKey(listing.mint!),
+    tokenSize: 1,
+    price: listing.price!
+  });
+
+  const txBuilder = await metaplex.auctionHouse().executeSale({
+    auctionHouse: auctionHouseObj,
+    sellerTradeState,
+    buyerTradeState,
+    tokenMint: new PublicKey(listing.mint!),
+    price: listing.price!,
+  });
+
+  const tx = new Transaction();
+  for (const ix of txBuilder.getInstructions()) {
+    tx.add(ix as TransactionInstruction);
+  }
+
+  const simResult = await connection.simulateTransaction(tx);
+  if (simResult.value.err) throw new Error(`Simulation failed: ${simResult.value.err}`);
+
+  return tx;
+}
