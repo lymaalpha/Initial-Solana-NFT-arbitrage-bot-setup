7404f16389c371ae310dc67c0ceefa7a985e66fd
lymaalpha <lymaalpha@gmail.com>
2025-10-17 05:03:41 +0200

    Update tensorMarketplace.ts

--- src/tensorMarketplace.ts
+++ src/tensorMarketplace.ts
@@ -1,28 +1,16 @@
-// src/tensorMarketplace.ts (DIAGNOSTIC CODE)
+// src/tensorMarketplace.ts (FINAL DIAGNOSTIC CODE)
 import * as TensorSDK from "@tensor-foundation/marketplace";
-import { pnlLogger } from "./pnlLogger";
 import { NFTListing, NFTBid } from "./types";
-import BN from "bn.js";
 
-// Log the entire SDK to see what functions are actually available.
-pnlLogger.logMetrics({
-  message: "DIAGNOSTIC: Logging contents of Tensor SDK",
-  sdkContents: Object.keys(TensorSDK),
-});
+// THIS IS THE ONLY THING THAT MATTERS.
+// It will print the contents of the SDK directly to the log.
+console.log("TENSOR_SDK_CONTENTS:", Object.keys(TensorSDK));
 
 // Return empty arrays to prevent the rest of the bot from crashing.
 export async function fetchListings(collectionId: string): Promise<NFTListing[]> {
-  pnlLogger.logMetrics({
-    message: "Tensor fetchListings is in diagnostic mode.",
-    collection: collectionId,
-  });
   return [];
 }
 
 export async function fetchBids(collectionId: string): Promise<NFTBid[]> {
-  pnlLogger.logMetrics({
-    message: "Tensor fetchBids is in diagnostic mode.",
-    collection: collectionId,
-  });
   return [];
 }
